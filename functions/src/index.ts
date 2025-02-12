2459ee2994a3d74c29e1c1e9851dd25da64b1355
Marnus Coetzee <marnuscoe@gmail.com>
2025-02-12 16:39:55 +0200

    Remove ESLint configuration for functions and implement user creation Firestore function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,28 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+admin.initializeApp(); // Make sure this is called once in your functions
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+exports.createUserDocument = functions.auth
+  .user()
+  .onCreate(async (user: any) => {
+    const { uid, email, displayName, photoURL, phoneNumber, providerData } =
+      user;
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+    const userData = {
+      uid,
+      email,
+      displayName,
+      photoURL,
+      phoneNumber,
+      providerData,
+      metadata: user.metadata,
+    };
+
+    try {
+      await admin.firestore().collection('users').doc(uid).set(userData);
+      console.log(`Successfully created Firestore doc for user: ${uid}`);
+    } catch (error) {
+      console.error(`Error creating Firestore doc for user: ${uid}`, error);
+    }
+  });
